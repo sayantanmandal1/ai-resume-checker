66695c733671e32928a96299749f222cf19eb171
Sayantan Mandal <msayantan05@gmail.com>
2025-06-03 13:41:27 +0530

    c

--- backend/tests/test_try1.py
+++ backend/tests/test_try1.py
@@ -12,8 +12,6 @@ from selenium.common.exceptions import TimeoutException, ElementClickIntercepted
 def driver():
     options = Options()
     options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
