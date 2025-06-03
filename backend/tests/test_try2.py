409db7c90cd9a6f1afc159a120bdc3651cc173eb
Sayantan Mandal <msayantan05@gmail.com>
2025-06-03 13:41:53 +0530

    k

--- backend/tests/test_try2.py
+++ backend/tests/test_try2.py
@@ -13,8 +13,6 @@ def driver():
     options = Options()
     options.add_argument("--headless=new")  # Uncomment for headless mode
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
